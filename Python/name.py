f255f9e8ef07d09a772a3ac3b625e619790ea3d6
Lakshika Parihar <31981299+lakshika1064@users.noreply.github.com>
2020-07-07 13:37:59 +0530

    Say "Hello, World!" With Python

--- Python/name.py
+++
@@ -1,5 +0,0 @@
-def print_full_name(a, b):
-    print("Hello",a,b,end='')
-    print("! You just delved into python.")
-
-
--- Python/print_function.py
+++
@@ -1,3 +0,0 @@
-k=int(input())
-for i in range(1,k+1):
-    print (i,end="")
